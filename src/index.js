3ba68020d16f1c3d88cba4f99e143e579532f76d
Noura <contact.nouraloudani@gmail.com>
2021-01-14 16:10:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  //create new task
+  const newTask = document.createElement("li");
+
+  //(html ligne 20) where the task li are stored
+  const taskUl = document.querySelector("#tasks");
+
+
+  //form const and description const
+  const newTaskForm = document.querySelector("#create-task-form");
+  const newTaskDescription = newTaskForm.querySelector("input");
+  //attach event listeners
+
+const createNewTask = event => {
+  event.preventDefault();
+  newTask.innerText = newTaskDescription.value;
+  taskUl.append(newTask);
+
+  event.target.reset();
+
+  };
+
+  newTaskForm.addEventListener('submit', createNewTask);
+
+
+
+
